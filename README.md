89dfb0d52b9078883e1550c47a6c0fe2c7608d53
marc-obe <marc.oberlin@bbzbl-it.ch>
2022-10-22 13:05:39 +0200

    Index & ReadME

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # M152
+
+## ***INSTALLATION GUIDE***
+
+1. **Install a webserver containing PHP and GD**
+   <br> Install a server yourself or choose a option below
+    - Windows: [Installation Guide xampp](https://www.wikihow.com/Install-XAMPP-for-Windows)
+    - MAC: [Installation Guide MAMP](https://documentation.mamp.info/en/MAMP-Mac/Installation/)
+    - For GD: [Installation Guide Php-Gd Windows](https://www.geeksforgeeks.org/how-to-install-php-gd-in-windows/)
+2. **Clone this Repository**
+   <br> use `git clone https://github.com/marc-obe/M152.git` to clone this Repository
+   <br> put the Repository in the `htdocs` folder from xampp/MAMP
+3. **Start**
+   <br> Start your new Webserver and you're good to go.
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body class=" bg-primary p-2 text-dark bg-opacity-25 ">
+<div class="container text-center">
+    <h1>Profile-Picture Maker</h1>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+
+        <div class="mb-3">
+            <label for="fileToUpload" class="form-label">Select image to upload</label>
+            <input type="file" name="fileToUpload" id="fileToUpload" class="form-control" required>
+
+        </div>
+        <input type="submit" value="Upload Image" name="submit" class="btn btn-primary">
+    </form>
+    <br>
+    <form action="download.php" method="post">
+        <figure>
+            <img src="bild.png" alt="bild">
+        </figure>
+        <input type="submit" name="submit" value="Download File" class="btn btn-primary"/>
+    </form>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
+
+
+</body>
+</html>
